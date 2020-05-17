32da558aa1f6d7f5fa6e605de501a0a2693e6d7f
Miroslaw Sztorc <mr.sztorc@gmail.com>
2020-05-17 15:31:29 +0200

    trait added

---
+++ src/Commands/Traits/CommandValidator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace msztorc\LaravelEnv\Commands\Traits;
+
+use InvalidArgumentException;
+
+trait CommandValidator {
+
+    /**
+     * Check if a given string is valid as an environment variable key.
+     *
+     * @param string $key
+     * @return boolean
+     */
+    protected function isValidKey(string $key): bool
+    {
+        if (!preg_match('/^[a-zA-Z_0-9]+$/', $key)) {
+            throw new InvalidArgumentException('Invalid environment key. Only use digits, letters and underscores');
+        }
+
+        return true;
+    }
+}
